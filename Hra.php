3c1cb9737cb6f3678851e7cc0df42d1fc3cfe4a9
MichaelCihak <156605123+MichaelCihak@users.noreply.github.com>
2024-01-14 21:03:54 +0100

    Create Hra.php
    Hra a herní pole

---
+++ Hra.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="./Scripty/herniPole.js"></script>
+    <script src="./Scripty/KontrolaVyhry.js"></script>
+    <link rel="stylesheet" type="text/css" href="./Styl/herniPoleStyl.css">
+    <title>Piškvorky</title>
+</head>
+<body id="hraBody">
+    <h1 id="nadpis">Piškvorky</h1>
+
+ <div id="moznosti">
+<label for="velikostPole">Vyberte velikost pole:</label>
+    <select id="velikostPole">
+        <option value="3">3x3</option>
+        <option value="4">4x4</option>
+        <option value="5">5x5</option>
+
+    </select>
+</div>
+</body>
+</html>
