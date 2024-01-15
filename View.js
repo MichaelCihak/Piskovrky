a13d01981bf2a1d0a93446587ba066ec1a98261e
MichaelCihak <156605123+MichaelCihak@users.noreply.github.com>
2024-01-15 18:32:29 +0100

    Update View.js
    formulář hotov

--- View.js
+++ View.js
@@ -19,7 +19,8 @@ constructor(){
     document.getElementById("registraceForm").appendChild(this.form);
   
 }
-  //vytvoření formuláře
+//vytvoření formuláře
+    
  createInput(id, type, placeholder, required) {
     var input = document.createElement("input");
     input.id = id;
@@ -28,6 +29,45 @@ constructor(){
     input.required = required;
     return input;
   }
-  
+
+// odeslání formuláře, event.preventDefault() - zabraňuje poslání dat, místo toho se volá metoda startGame()
+    
+  onSubmit(event){
+      event.preventDeafult();
+      this.startGame();
+  }
+// získání hodnot jmen z formuláře
+startGame(){
+        let jmeno1 = document.getElementById("jmeno1").value;
+    let jmeno2 = document.getElementById("jmeno2").value;
+
+    let data = {
+      jmeno1: jmeno1,
+      jmeno2: jmeno2
+    };
+    // asynchronní odeslání dat XMLHttpRequest
+    
+    let ajax = new XMLHttpRequest();
+    ajax.open("POST", "pridatDoSession.php", true);
+    ajax.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+    ajax.onreadystatechange = function() {
+      if (ajax.readyState == 4 && ajax.status == 200) {
+        console.log(ajax.responseText);
+      }
+    };
+
+    ajax.send(JSON.stringify(data));
+  }
 }
 
+function go(){
+      var jmeno1 = document.getElementById("jmeno1").value;
+  var jmeno2 = document.getElementById("jmeno2").value;
+
+  if (jmeno1 === "" || jmeno2 === "") {
+    alert("Prosím, vyplňte obě jména před registrací do hry.");
+  } else {
+    window.location.href = "./Hra.php";
+  }
+}
