f5efe7b7978e8fc3acb1b6fe3c5afa91b78aac23
MichaelCihak <156605123+MichaelCihak@users.noreply.github.com>
2024-01-15 18:12:58 +0100

    Update View.js
    registrační formulář

--- View.js
+++ View.js
@@ -1,1 +1,33 @@
+class RegistracniForm{
+constructor(){
+    this.form = document.createElement("form");
+    this.form.id = "dynamicForm";
+    this.form.onsubmit = this.onSubmit.bind(this);
+
+    this.inputJmeno1 = this.createInput("jmeno1", "text", "jmeno hrace 1", true);
+    this.inputJmeno2 = this.createInput("jmeno2", "text", "jmeno hrace 2", true);
+
+    this.buttonRegistrace = document.createElement("input");
+    this.buttonRegistrace.type = "submit";
+    this.buttonRegistrace.value = "Registrace";
+    this.buttonRegistrace.id = "registrace";
+
+    this.form.appendChild(this.inputJmeno1);
+    this.form.appendChild(this.inputJmeno2);
+    this.form.appendChild(this.buttonRegistrace);
+
+    document.getElementById("registraceForm").appendChild(this.form);
+  
+}
+  //vytvoření formuláře
+ createInput(id, type, placeholder, required) {
+    var input = document.createElement("input");
+    input.id = id;
+    input.type = type;
+    input.placeholder = placeholder;
+    input.required = required;
+    return input;
+  }
+  
+}
 
