1ea125e8e4c682cb758d32bc24473c21552ebb52
MichaelCihak <156605123+MichaelCihak@users.noreply.github.com>
2024-01-16 20:15:32 +0100

    Update herniPole.js
    generování pole a příprava na vkládání symbolů

--- herniPole.js
+++ herniPole.js
@@ -3,4 +3,51 @@ class HerniPole(){
 
     
   }
+
+    generovatPole() {
+      var velikost = parseInt(document.getElementById("velikostPole").value);
+      var herniPole = document.getElementById("herniPole");
+  
+      herniPole.innerHTML = '';
+      herniPole.style.gridTemplateColumns = `repeat(${velikost}, 100px)`;
+      herniPole.style.gridTemplateRows = `repeat(${velikost}, 100px)`;
+  
+      for (var i = 0; i < velikost * velikost; i++) {
+        var cell = document.createElement("div");
+        cell.classList.add("piskvorky-cell");
+        cell.setAttribute("data-index", i);
+        cell.innerHTML = " ";
+        cell.addEventListener("click", (event) => this.umistitSymbol(event));
+        herniPole.appendChild(cell);
+      }
+    }
+
+novaHra(herniPole, velikostPoleInput) {
+      this.hraSkoncila = false;
+      this.aktualniHracIndex = 0;
+      var velikost = parseInt(velikostPoleInput.value);
+
+// vyprázdnění pole a nastavení stylu buněk
+  
+      herniPole.innerHTML = '';
+      herniPole.style.gridTemplateColumns = `repeat(${velikost}, 100px)`;
+      herniPole.style.gridTemplateRows = `repeat(${velikost}, 100px)`;
+
+// tvorba div elementů pro každou buňku
+  
+      for (var i = 0; i < velikost * velikost; i++) {
+        var cell = document.createElement("div");
+        cell.classList.add("piskvorky-cell");
+        cell.setAttribute("data-index", i);
+        cell.innerHTML = " ";
+        cell.addEventListener("click", (event) => this.umistitSymbol(event));
+        herniPole.appendChild(cell);
+      }
+    }
+
+  umistitSymbol(event){
+    
+  }
+
+  
 }
