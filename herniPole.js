b2abcdacd3d564fca203be6a60d52a9cbbef700b
MichaelCihak <156605123+MichaelCihak@users.noreply.github.com>
2024-01-18 19:31:37 +0100

    Update herniPole.js
    počítání výher

--- herniPole.js
+++ herniPole.js
@@ -3,10 +3,11 @@ class HerniPole{
   this.hraci = ["X", "O"];
   this.aktualniHracIndex = 0;
   this.hraSkoncila = false;
-    
+  this.hrac1Vyhry = 0;
+  this.hrac2Vyhry = 0;  
   }
 
-    generovatPole() {
+generovatPole() {
       var velikost = parseInt(document.getElementById("velikostPole").value);
       var herniPole = document.getElementById("herniPole");
   
@@ -22,6 +23,10 @@ class HerniPole{
         cell.addEventListener("click", (event) => this.umistitSymbol(event));
         herniPole.appendChild(cell);
       }
+      // reset hry
+      
+      this.hraSkoncila = false;
+      this.aktualniHracIndex = 0;
     }
 
 novaHra(herniPole, velikostPoleInput) {
@@ -47,7 +52,7 @@ novaHra(herniPole, velikostPoleInput) {
       }
     }
 
-  umistitSymbol(event){
+umistitSymbol(event){
       if (this.hraSkoncila) {
         return;
       }
@@ -55,6 +60,7 @@ novaHra(herniPole, velikostPoleInput) {
       var index = event.target.getAttribute("data-index");
       this.umistitSymbolPodleIndexu(index);
   }
+  
  umistitSymbolPodleIndexu(index) {
       var ctverec = document.querySelector(`[data-index="${index}"]`);
   
@@ -62,5 +68,36 @@ novaHra(herniPole, velikostPoleInput) {
         ctverec.innerHTML = this.hraci[this.aktualniHracIndex];
         this.aktualniHracIndex = (this.aktualniHracIndex + 1) % this.hraci.length;
       }
+        if (this.kontrolaVyhry()) {
+          console.log("Hra skončila, hráč " + this.hraci[this.aktualniHracIndex ^ 1] + " vyhrál!");
+          this.hraSkoncila = true;
+  
+          // přiřazování výher
+          if (this.hraci[this.aktualniHracIndex ^ 1] === "X") {
+            this.hrac1Vyhry++;
+          } else if (this.hraci[this.aktualniHracIndex ^ 1] === "O") {
+            this.hrac2Vyhry++;
+          }
+  
+          this.aktualizovatPocetVyhry();
+        }
    }
+
+  aktualizovatPocetVyhry() {
+        var hrac1VyhryElement = document.getElementById("hrac1-vyhry");
+        var hrac2VyhryElement = document.getElementById("hrac2-vyhry");
+    
+        if (hrac1VyhryElement && hrac2VyhryElement) {
+            hrac1VyhryElement.innerText = "Hráč 1: " + this.hrac1Vyhry;
+            hrac2VyhryElement.innerText = "Hráč 2: " + this.hrac2Vyhry;
+        } else {
+            console.error("Elementy pro počet výher nebyly nalezeny.");
+        }
+    }
+  
+  kontrolaVyhry() {
+      var kontrola = new KontrolaVyhry();
+      return kontrola.kontrolaVyhry();
+    }
+  
 }
